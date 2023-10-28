1b96a4132070ec93a317d2a35632068bb9020be9
Olegreg762 <109924953+Olegreg762@users.noreply.github.com>
2023-10-28 05:36:47 -0500

    create Nav bar(styling not started)

---
+++ portfolio/src/components/Nav.jsx
@@ -0,0 +1,26 @@
+export default function Nav() {
+    
+    return (
+        <nav>
+            <section style={{
+
+            }}>
+                <ul>
+                    {/* <!-- Links for page navigation --> */}
+                    <li><a href="#about_me">About_Me</a></li>
+                    <li><a href="#contact_h4">Contact</a></li>
+                    <li><a href="https://www.linkedin.com/in/greg-stevenson-422931a9/">Linkedin</a></li>
+                    <li><a href="https://github.com/Olegreg762/">Github</a></li>
+                    <li><a href="#work">My_Work</a></li>:
+                    <li><a href="#python_h3">Python</a></li>
+                    <li><a href="#sql_h3">SQL</a></li>
+                    <li><a href="#solidity_h3">Solidity</a></li>
+                    <li><a href="#javascript_h3">Javascript</a></li>
+                    <li><a href="#css_h3">CSS</a></li>
+                    <li><a href="#html_h3">HTML</a></li>
+                    <li><a href="handlebars_h3">Handlebars</a></li>
+                </ul>
+            </section>
+        </nav>
+    );
+}
